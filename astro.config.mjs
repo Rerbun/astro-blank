22e0f9a87ca71567b4470a7c3d2242c2c8c9f3a0
Ruben <Rerbun@users.noreply.github.com>
2024-03-09 23:31:02 +0000

    Reverted Astro config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  server: { port: 3000, host: true }
-});
+export default defineConfig({});
